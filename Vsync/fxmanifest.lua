758ad4b4dfc06d27baa42cd9ecda14fbb30dd34d
ALEXAIORUSHUN <fsccasebox@gmail.com>
2024-12-01 19:02:51 +0100

    Vsync
    Egy egyszerűsített időjárás script

---
+++ Vsync/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Codesign#2715'
+description 'Időjárás, Idő configurációja'
+version '1.3.7'
+
+client_script 'VSync/vs_client.lua'
+
+server_scripts {
+    'VSync/vs_server.lua'
+}
+
+provide 'vSync'
